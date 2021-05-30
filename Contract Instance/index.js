ee3bca03aaeebf3703d1e970d4edd7513a2e7308
DebjitPramanick <dpramanick9990@gmail.com>
2021-05-30 08:25:29 +0530

    Contract Instance

--- Contract Instance/index.js
+++ Contract Instance/index.js
@@ -1,20 +1,29 @@
-const Web3 = require('web3') 
+const Web3 = require('web3')
 const MyContract = require('./build/contracts/MyContract.json')
 
 
 // Contract connected with web3
 
 const init = async () => {
-    const web3 = new Web3('http://localhost:8545')
-
+    const web3 = new Web3('http://localhost:7545')
     const id = await web3.eth.net.getId()
     const deployedNetwork = MyContract.networks[id]
+
     const contract = new web3.eth.Contract(
         MyContract.abi,
         deployedNetwork.address
     );
 
+
+    // To get all the addresses
+    const addresses = await web3.eth.getAccounts()
+
     // Calling a function
+
+    contract.methods.setData(10).send(
+        { from: addresses[0] }
+    ).then(res => res)
+
     contract.methods.getData().call()
     .then(res => console.log(res))
 }
